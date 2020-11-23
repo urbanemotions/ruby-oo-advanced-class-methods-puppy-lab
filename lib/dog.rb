31e6644fa3664a2d3611bc0e01d6547ae47affa9
sin wan wildy <sw.wildy@gmail.com>
2020-11-23 10:27:32 -0800

    commit tree 1b36c267275b7c7c7157999b1b7da67bde03ebd5

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,28 @@
-# Add your code here
+class Dog
+    
+    @@all = []
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def self.all 
+        @@all
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.print_all
+        puts @@all.map{|array| array.name} 
+    end
+
+    def self.clear_all
+        @@all.clear 
+    end
+
+end
---
+++ lib/run.rb
@@ -0,0 +1,7 @@
+require 'pry'
+require_relative "../lib/dog.rb"
+
+d1 = Dog.new("Sin")
+
+binding.pry
+0
